e96be62369b8d0cfe6881935abf8fc2a854908e9
rish-16 <mail.rishabh.anand@gmail.com>
2021-06-25 12:42:45 +0800

    Add custom projection layer for env state

--- dreamerv2/model.py
+++ dreamerv2/model.py
@@ -2,31 +2,36 @@ import torch
 import torch.nn as nn
 
 class WorldModel(nn.Module):
-    def __init__(self, gamma, num_action=9):
+    def __init__(self, gamma, num_action=18):
         super(WorldModel, self).__init__()
         self.gamma = gamma #discount factor
         self.num_action = num_action
 
         #Recurrent Model (RSSM): ((z, a), h) -> h
-        self.gru = nn.GRUCell(
-            input_size=1024 + num_action,
-            hidden_size=512,
-        )
+        self.gru = nn.GRUCell(input_size=1024 + num_action, hidden_size=512)
 
-        #q (1st part): (x) -> xembedded
-        self.representation_model_conv = nn.Sequential(
-            #1,64,64
-            nn.Conv2d(3, 32, 3, padding=1, stride=2), #32,32,32
+        # #q (1st part): (x) -> xembedded
+        # self.representation_model_encoder = nn.Sequential(
+        #     #1,64,64
+        #     nn.Conv2d(3, 32, 3, padding=1, stride=2), #32,32,32
+        #     nn.ELU(inplace=True),
+        #     nn.Conv2d(32, 64, 3, padding=1, stride=2), #64,16,16
+        #     nn.ELU(inplace=True),
+        #     nn.Conv2d(64, 128, 3, padding=1, stride=2), #128, 8, 8
+        #     nn.ELU(inplace=True),
+        #     nn.Conv2d(128, 256, 3, padding=1, stride=2), #256, 4, 4
+        #     nn.ELU(inplace=True),
+        #     nn.Conv2d(256, 512, 4), #512, 1, 1
+        #     nn.ELU(inplace=True)
+        # )
+        
+        self.representation_model_encoder = nn.Sequential(
+            nn.Linear(128, 256),
             nn.ELU(inplace=True),
-            nn.Conv2d(32, 64, 3, padding=1, stride=2), #64,16,16
-            nn.ELU(inplace=True),
-            nn.Conv2d(64, 128, 3, padding=1, stride=2), #128, 8, 8
-            nn.ELU(inplace=True),
-            nn.Conv2d(128, 256, 3, padding=1, stride=2), #256, 4, 4
-            nn.ELU(inplace=True),
-            nn.Conv2d(256, 512, 4), #512, 1, 1
-            nn.ELU(inplace=True)
+            nn.Linear(256, 512),
+            nn.Dropout(0.5),
         )
+        
         #q (2nd part): (h, xembedded) -> z
         self.representation_model_mlp = nn.Sequential(
             nn.Linear(512+512, 1024),
@@ -65,29 +70,31 @@ class WorldModel(nn.Module):
             nn.Linear(1024+512, 1024),
             nn.ELU(inplace=True),
         )
-        self.image_predictor_conv = nn.Sequential( #64, 4, 4
-            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1), #32, 8, 8
+        
+        # self.image_predictor_conv = nn.Sequential( #64, 4, 4
+        #     nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1), #32, 8, 8
+        #     nn.ELU(inplace=True),
+        #     nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1), #16, 16, 16
+        #     nn.ELU(inplace=True),
+        #     nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1), #8, 32, 32
+        #     nn.ELU(inplace=True),
+        #     nn.ConvTranspose2d(8, 8, 3, stride=2, padding=1, output_padding=1), #3, 64, 64
+        #     nn.ELU(inplace=True),
+        #     nn.Conv2d(8, 3, 3, padding=1),
+        # )
+        
+        self.state_predictor_decoder = nn.Sequential(
+            nn.Linear(1024, 512),
             nn.ELU(inplace=True),
-            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1), #16, 16, 16
+            nn.Linear(512, 16),
             nn.ELU(inplace=True),
-            nn.ConvTranspose2d(16, 8, 3, stride=2, padding=1, output_padding=1), #8, 32, 32
+            nn.Linear(16, 8),
             nn.ELU(inplace=True),
-            nn.ConvTranspose2d(8, 8, 3, stride=2, padding=1, output_padding=1), #3, 64, 64
-            nn.ELU(inplace=True),
-            nn.Conv2d(8, 3, 3, padding=1),
+            nn.Linear(8, 8),
+            nn.ELU(inplace=True)
         )
     
     def compute_h(self, batch_size, device, a=None, h=None, z=None):
-        """
-        In:
-            batch_size: int
-            device: torch.device of the model
-            a: a_t-1
-            h: h_t-1
-            z: z_t-1
-        Out:
-            h: h_t
-        """
         if h is None: #starting new sequence
             h = torch.zeros((batch_size, 512)).to(device)
         else:
@@ -104,62 +111,40 @@ class WorldModel(nn.Module):
             z_logit:  logits of z_t
             z_sample: z_t
         """
-        embedding = self.representation_model_conv(x)
+        embedding = self.representation_model_encoder(x)
         embedding = embedding.reshape(-1, 512)
         embedding = torch.cat((h, embedding), dim=1)
         z_logits = self.representation_model_mlp(embedding)
-        z_sample = torch.distributions.one_hot_categorical.OneHotCategorical(
-            logits=z_logits.reshape(-1, 32, 32)
-        ).sample()
+        z_sample = torch.distributions.one_hot_categorical.OneHotCategorical(logits=z_logits.reshape(-1, 32, 32)).sample()
         z_probs = torch.softmax(z_logits.reshape(-1, 32, 32), dim=-1)
         z_sample = z_sample + z_probs - z_probs.detach()
 
         return z_logits, z_sample
     
     def compute_z_hat_sample(self, z_hat_logits):
-        """
-        In:
-            z_hat_logits: logits of z_hat_t
-        Out:
-            z_hat_sample (with straight through gradient)
-        """
-        z_hat_sample = torch.distributions.one_hot_categorical.OneHotCategorical(
-            logits=z_hat_logits.reshape(-1, 32, 32)
-        ).sample()
+        z_hat_sample = torch.distributions.one_hot_categorical.OneHotCategorical(logits=z_hat_logits.reshape(-1, 32, 32)).sample()
         z_hat_probs = torch.softmax(z_hat_logits.reshape(-1, 32, 32), dim=-1)
         
         return z_hat_sample + z_hat_probs - z_hat_probs.detach()
 
     def compute_x_hat(self, h_z):
-        """
-        In:
-            h_z: concat of h_t and z_t
-        Out:
-            x_hat: x_hat_t
-        """
         x_hat = self.x_hat_predictor_mlp(h_z)
-        x_hat = x_hat.reshape(-1, 64, 4, 4)
-        return self.image_predictor_conv(x_hat)
+        # x_hat = x_hat.reshape(-1, 64, 4, 4)
+        return self.state_predictor_decoder(nn.Flatten()(x_hat))
 
     #using inference
     def forward_inference(self, a, x, z, h):
-        """
-        In:
-            a: a_t-1
-            x: x_t
-            z: z_t-1 (sampled not logits)
-            h: h_t-1
-        """
         h = self.compute_h(x.shape[0], x.device, a, h, z)
 
-        embedding = self.representation_model_conv(x)
+        embedding = self.representation_model_encoder(x)
         embedding = embedding.reshape(-1, 512)
         embedding = torch.cat((h, embedding), dim=1)
         z_logits = self.representation_model_mlp(embedding)
         z_sample = torch.distributions.one_hot_categorical.OneHotCategorical(
             logits=z_logits.reshape(-1, 32, 32)
         ).sample()
-        #no straight-though gradient
+        
+        # no straight-though gradient
         return z_sample, h
 
     def dream(self, a, x, z, h):
@@ -210,13 +195,6 @@ class WorldModel(nn.Module):
 
 
     def forward(self, a, x, z, h=None, dream=False, inference=False):
-        """
-        Input:
-            a: a_t-1
-            x: x_t
-            z: z_t-1
-            h: h_t-1
-        """
         if inference: #only use embedding network, i.e. no image predictor
             return self.forward_inference(a, x, z, h)
         elif dream:
@@ -270,31 +248,21 @@ class LossModel(nn.Module):
         self.nq = nq
 
     def forward(self, x, r, gamma, z_logits, z_sample, x_hat, r_hat, gamma_hat, z_hat_logits):
-        x_dist = torch.distributions.normal.Normal(
-            loc=x_hat,
-            scale=1.0
-        )
-        r_dist = torch.distributions.normal.Normal(
-            loc=r_hat,
-            scale=1.0
-        )
-        gamma_dist = torch.distributions.bernoulli.Bernoulli(
-            logits=gamma_hat
-        )
-        z_hat_dist = torch.distributions.one_hot_categorical.OneHotCategorical(
-            logits=z_hat_logits.reshape(-1, 32, 32)
-        )
-        z_dist = torch.distributions.one_hot_categorical.OneHotCategorical(
-            logits=z_logits.reshape(-1, 32, 32).detach()
-        )
-
+        x_dist = torch.distributions.normal.Normal(loc=x_hat, scale=1.0)
+        r_dist = torch.distributions.normal.Normal(loc=r_hat, scale=1.0)
+        gamma_dist = torch.distributions.bernoulli.Bernoulli(logits=gamma_hat)
+        z_hat_dist = torch.distributions.one_hot_categorical.OneHotCategorical(logits=z_hat_logits.reshape(-1, 32, 32))
+        z_dist = torch.distributions.one_hot_categorical.OneHotCategorical(logits=z_logits.reshape(-1, 32, 32).detach())
+        
+        print (z_sample.shape)
         z_sample = z_sample.reshape(-1, 32, 32)
+        print (z_sample.shape)
 
-        loss = -self.nx*x_dist.log_prob(x).mean()\
-                -self.nr*r_dist.log_prob(r).mean()\
-                -self.ng*gamma_dist.log_prob(gamma.round()).mean()\
-                -self.nt*z_hat_dist.log_prob(z_sample.detach()).mean()\
-                +self.nq*z_dist.log_prob(z_sample).mean()
+        loss = -self.nx*x_dist.log_prob(x).mean().item() \
+                -self.nr*r_dist.log_prob(r).mean().item() \
+                -self.ng*gamma_dist.log_prob(gamma.round()).mean().item() \
+                -self.nt*z_hat_dist.log_prob(z_sample.detach()).mean().item() \
+                +self.nq*z_dist.log_prob(z_sample).mean().item()
 
         return loss
 
@@ -309,6 +277,11 @@ class ActorLoss(nn.Module):
         self.anneal = 1e-5
 
     def forward(self, a, dist_a, V, ve):
+        print (a.shape)
+        print (dist_a.shape)
+        print (V.shape)
+        print (ve.shape)
+        
         loss = -self.ns * dist_a.log_prob(a) * (V - ve).detach().squeeze(-1)\
             -self.nd * V.squeeze(-1)\
             -self.ne * dist_a.entropy()
--- dreamerv2/train.py
+++ dreamerv2/train.py
@@ -14,37 +14,41 @@ import torchvision
 
 from dataset import ModelDataset
 from model import WorldModel, Actor, Critic, LossModel, ActorLoss, CriticLoss
-# plt.ion()
 
-
-### HYPERPARAMETERS ###
+### HYPERPARAMETERS ### 
 save_path = "save.chkpt"
-env_name = "Qbert-v0"
-env = gym.make(env_name, frameskip = 4)
+env_name = "BreakoutNoFrameskip-v4"
+env = gym.make(env_name, obs_type="ram", full_action_space=True)
+# env = gym.make(env_name, frameskip = 4)
 num_actions = env.action_space.n
 
 batch = 64
-L = 50 #seq len world training
+L = 50 # seq len world training
 history_size = 64
 lr_world = 2e-4
 
-H = 15 #imagination length
-gamma = 0.995 #discount factor
-lamb = 0.95 #lambda-target
+H = 15 # imagination length
+gamma = 0.995 # discount factor
+lamb = 0.95 # lambda-target
 lr_actor = 4e-5
 lr_critic = 1e-4
-target_interval = 100 #update interval for target critic
+target_interval = 100 # update interval for target critic
 
 gradient_clipping = 100
 adam_eps = 1e-5
 decay = 1e-6
 
-
-### MODELS ###
-world = WorldModel(gamma, num_actions).cuda()
-actor = Actor(num_actions).cuda()
-critic = Critic().cuda()
-target = Critic().cuda()
+###  MODELS ###
+if torch.cuda.is_available():
+    world = WorldModel(gamma, num_actions).cuda()
+    actor = Actor(num_actions).cuda()
+    critic = Critic().cuda()
+    target = Critic().cuda()
+else:
+    world = WorldModel(gamma, num_actions)
+    actor = Actor(num_actions)
+    critic = Critic()
+    target = Critic()
 
 criterionModel = LossModel()
 criterionActor = ActorLoss()
@@ -66,11 +70,19 @@ if os.path.isfile(save_path):
         optim_critic.load_state_dict(w["optim_critic"])
         criterionActor = ActorLoss(*w["criterionActor"])
     except:
-        print("error loading model")
-        world = WorldModel(gamma, num_actions).cuda()
-        actor = Actor(num_actions).cuda()
-        critic = Critic().cuda()
-        target = Critic().cuda()
+        print ("error loading model")
+        
+        if torch.cuda.is_available():
+            world = WorldModel(gamma, num_actions).cuda()
+            actor = Actor(num_actions).cuda()
+            critic = Critic().cuda()
+            target = Critic().cuda()
+        else:
+            world = WorldModel(gamma, num_actions)
+            actor = Actor(num_actions)
+            critic = Critic()
+            target = Critic()
+            
         criterionModel = LossModel()
         criterionActor = ActorLoss()
         criterionCritic = CriticLoss()
@@ -79,26 +91,24 @@ if os.path.isfile(save_path):
         optim_critic = Adam(critic.parameters(), lr=lr_critic, eps=adam_eps, weight_decay=decay)
         optim_target = Adam(target.parameters())
     del w
+    
 with torch.no_grad():
     target.load_state_dict(critic.state_dict())
 
-
 ### MISC ###
-resize = torchvision.transforms.Resize(
-    (64,64),
-    interpolation=PIL.Image.BICUBIC
-)
-grayscale = torchvision.transforms.Grayscale()
+#  resize = torchvision.transforms.Resize(
+#      (64,64),
+#      interpolation=PIL.Image.BICUBIC
+#  )
+#  grayscale = torchvision.transforms.Grayscale()
 def transform_obs(obs):
-    obs = resize(
-        torch.from_numpy(obs.transpose(2,0,1))
-    ) #3, 64, 64
+    #  obs = resize(torch.from_numpy(obs.transpose(2,0,1))) #  3, 64, 64
+    obs = torch.from_numpy(obs)
     return (obs.float() - 255/2).unsqueeze(0)
+
 history = []
 tensor_range = torch.arange(0, num_actions).unsqueeze(0)
-random_action_dist = torch.distributions.one_hot_categorical.OneHotCategorical(
-    torch.ones((1,num_actions))
-)
+random_action_dist = torch.distributions.one_hot_categorical.OneHotCategorical(torch.ones((1, num_actions)))
 
 def gather_episode():
     with torch.no_grad():
@@ -106,35 +116,41 @@ def gather_episode():
             obs = env.reset()
             obs = transform_obs(obs)
             episode = [obs]
-            obs = obs.cuda()
+            if (torch.cuda.is_available()):
+                obs = obs.cuda()
+
             z_sample, h = world(None, obs, None, inference=True)
             done = False
+            
             while not done:
-                env.render()
+                #  env.render()
                 a = actor(z_sample)
-                a = torch.distributions.one_hot_categorical.OneHotCategorical(
-                    logits = a
-                ).sample()
-                obs, rew, done, _ = env.step(int((a.cpu()*tensor_range).sum().round())) # take a random action (int)
+                a = torch.distributions.one_hot_categorical.OneHotCategorical(logits = a).sample()
+                obs, rew, done, _ = env.step(int((a.cpu()*tensor_range).sum().round())) #  take a random action (int)
                 obs = transform_obs(obs)
-                obs = obs.cuda()
+                
+                if (torch.cuda.is_available()):
+                    obs = obs.cuda()
+                
                 episode.extend([a.cpu(), tanh(rew), done, obs.cpu()])
+                
                 if not done:
                     z_sample, h = world(a, obs, z_sample.reshape(-1, 1024), h, inference=True)
-                # plt.imshow(obs[0].cpu().numpy().transpose(1,2,0)/2+0.5)
-                # plt.show()
+                
+                #  plt.imshow(obs[0].cpu().numpy().transpose(1,2,0)/2+0.5)
+                #  plt.show()
             history.append(episode)
             for _ in range(len(history) - history_size):
                 history.pop(0)
 
-#start gathering episode thread
+# start gathering episode thread
 t = threading.Thread(target=gather_episode)
 t.start()
 
-print("Dataset init")
+print ("Dataset init")
 while len(history) < 1:
     pass
-print("done")
+print ("done")
 
 def act_straight_through(z_hat_sample):
     a_logits = actor(z_hat_sample)
@@ -159,56 +175,30 @@ loader = torch.utils.data.DataLoader(
 
 iternum = 0
 start = time()
+
 while True:
     pbar = tqdm(loader)
     for s, a, r, g in pbar:
-        s = s.cuda()
-        a = a.cuda()
-        r = r.cuda()
-        g = g.cuda()
+        if (torch.cuda.is_available()):
+            s = s.cuda()
+            a = a.cuda()
+            r = r.cuda()
+            g = g.cuda()
+            
         z_list = []
         h_list = []
 
-        ### Train world model ###
-        z_logit, z_sample, z_hat_logits, x_hat, r_hat, gamma_hat, h, _ = world(
-            a=None,
-            x=s[:,0],
-            z=None,
-            h=None
-        )
-        loss_model = criterionModel(
-            s[:,0],
-            r[:,0], #false r_0 does not exist, 0: t=1 but expect 0:t=0
-            g[:,0], #same but ok since never end of episode
-            z_logit,
-            z_sample,
-            x_hat,
-            0, #rhat
-            gamma_hat,
-            z_hat_logits
-        )
+        # # #  Train world model # # # 
+        z_logit, z_sample, z_hat_logits, x_hat, r_hat, gamma_hat, h, _ = world(a=None, x=s[:,0], z=None, h=None)
+        loss_model = criterionModel(s[:,0], r[:,0], g[:,0], z_logit, z_sample, x_hat, 0, gamma_hat, z_hat_logits)
         z_list.append(z_sample.detach())
         h_list.append(h.detach())
+        
         for t in range(L):
-            z_logit, z_sample, z_hat_logits, x_hat, r_hat, gamma_hat, h, _ = world(
-                a[:,t],
-                s[:,t+1],
-                z_sample,
-                h
-            )
+            z_logit, z_sample, z_hat_logits, x_hat, r_hat, gamma_hat, h, _ = world(a[:,t], s[:,t+1], z_sample, h)
             z_list.append(z_sample.detach())
             h_list.append(h.detach())
-            loss_model += criterionModel(
-                s[:,t+1],
-                r[:,t], #r time array starts at 1; 0: t=1
-                g[:,t], #g time array starts at 1; 0: t=1
-                z_logit,
-                z_sample,
-                x_hat,
-                r_hat,
-                gamma_hat,
-                z_hat_logits
-            )
+            loss_model += criterionModel(s[:,t+1], r[:,t], g[:,t], z_logit, z_sample, x_hat, r_hat, gamma_hat, z_hat_logits)
         
         loss_model /= L
         loss_model.backward()
@@ -216,64 +206,53 @@ while True:
         optim_model.step()
         optim_model.zero_grad()
 
-        ### Train actor critic ###
-        #store every value to compute V since we sum backwards
+        # # #  Train actor critic # # # 
+        # store every value to compute V since we sum backwards
         r_hat_sample_list = []
         gamma_hat_sample_list = []
         a_sample_list = []
         a_logits_list = []
 
-        z_hat_sample = torch.cat(z_list, dim=0).detach() #convert all z to z0, squash time dim
+        z_hat_sample = torch.cat(z_list, dim=0).detach() # convert all z to z0, squash time dim
         z_hat_sample_list = [z_hat_sample]
 
-        h = torch.cat(h_list, dim=0).detach() #get corresponding h0
+        h = torch.cat(h_list, dim=0).detach() # get corresponding h0
         
-        # store values
+        #  store values
         for _ in range(H):
             a_sample, a_logits = act_straight_through(z_hat_sample)
 
-            *_, h, (z_hat_sample, r_hat_sample, gamma_hat_sample) = world(
-                a_sample,
-                x = None,
-                z = z_hat_sample.reshape(-1, 1024),
-                h = h,
-                dream=True
-            )
+            *_, h, (z_hat_sample, r_hat_sample, gamma_hat_sample) = world(a_sample, x = None, z = z_hat_sample.reshape(-1, 1024), h = h, dream=True)
             r_hat_sample_list.append(r_hat_sample)
             gamma_hat_sample_list.append(gamma_hat_sample)
             z_hat_sample_list.append(z_hat_sample)
             a_sample_list.append(a_sample)
             a_logits_list.append(a_logits)
 
-        # calculate paper recursion by looping backward 
-        V = r_hat_sample_list[-1] + gamma_hat_sample_list[-1] * target(z_hat_sample_list[-1]) #V_H-1
+        #  calculate paper recursion by looping backward 
+        V = r_hat_sample_list[-1] + gamma_hat_sample_list[-1] * target(z_hat_sample_list[-1]) # V_H-1
         ve = critic(z_hat_sample_list[-2].detach())
         loss_critic = criterionCritic(V.detach(), ve)
         loss_actor = criterionActor(
             a_sample_list[-1],
-            torch.distributions.one_hot_categorical.OneHotCategorical(
-                logits=a_logits_list[-1]
-            ),
-            V,
-            ve.detach()
+            torch.distributions.one_hot_categorical.OneHotCategorical(logits=a_logits_list[-1]),
+            V, ve.detach()
         )
+        
         for t in range(H-2, -1, -1):
             V = r_hat_sample_list[t] + gamma_hat_sample_list[t] * ((1-lamb)*target(z_hat_sample_list[t+1]) + lamb*V)
             ve = critic(z_hat_sample_list[t].detach())
             loss_critic += criterionCritic(V.detach(), ve)
             loss_actor += criterionActor(
                 a_sample_list[t],
-                torch.distributions.one_hot_categorical.OneHotCategorical(
-                    logits=a_logits_list[t]
-                ),
-                V,
-                ve.detach()
+                torch.distributions.one_hot_categorical.OneHotCategorical(logits=a_logits_list[t]),
+                V, ve.detach()
             )
 
         loss_actor /= (H-1)
         loss_critic /= (H-1)
 
-        #update actor
+        # update actor
         loss_actor.backward()
         loss_critic.backward()
         torch.nn.utils.clip_grad_norm_(actor.parameters(), gradient_clipping)
@@ -281,19 +260,19 @@ while True:
         optim_actor.zero_grad()
         optim_model.zero_grad()
 
-        #update critic
+        # update critic
         torch.nn.utils.clip_grad_norm_(critic.parameters(), gradient_clipping)
         optim_critic.step()
         optim_critic.zero_grad()
         optim_target.zero_grad()
 
-        #update target network with critic weights
+        # update target network with critic weights
         iternum += 1
         if not iternum % target_interval:
             with torch.no_grad():
                 target.load_state_dict(critic.state_dict())
         
-        # display
+        #  display
         pbar.set_postfix(
             l_world = loss_model.item(),
             l_actor = loss_actor.item(),
@@ -302,33 +281,36 @@ while True:
             iternum=iternum,
             last_rew=sum(history[-1][2::4]),
         )
-        print(a_logits_list[0][0].detach())
-        print(list(z_hat_sample_list[-1][0,1].detach().cpu().numpy().round()).index(1))
+        
+        print (a_logits_list[0][0].detach())
+        print (list(z_hat_sample_list[-1][0,1].detach().cpu().numpy().round()).index(1))
 
-    #save once in a while
+    # save once in a while
     if time() - start > 1*60:
         start = time()
-        print("Saving...")
+        print ("Saving...")
+        
         torch.save(
             {
-                "world":world.state_dict(),
-                "actor":actor.state_dict(),
-                "critic":critic.state_dict(),
-                "optim_model":optim_model.state_dict(),
-                "optim_actor":optim_actor.state_dict(),
-                "optim_critic":optim_critic.state_dict(),
+                "world": world.state_dict(),
+                "actor": actor.state_dict(),
+                "critic": critic.state_dict(),
+                "optim_model": optim_model.state_dict(),
+                "optim_actor": optim_actor.state_dict(),
+                "optim_critic": optim_critic.state_dict(),
                 "criterionActor": (criterionActor.ns, criterionActor.nd, criterionActor.ne),
             },
             save_path
         )
-        print("...done")
-        plt.imsave("img.png", np.clip((x_hat[0].detach().cpu().numpy().transpose(1,2,0))/255+0.5, 0, 1))
+        
+        print ("...done")
+        #  plt.imsave("img.png", np.clip((x_hat[0].detach().cpu().numpy().transpose(1,2,0))/255+0.5, 0, 1))
 
-    # plt.figure(1)
-    # # plt.clf()
-    # plt.imshow(x_hat[0].detach().cpu().numpy().transpose(1,2,0), cmap='gray')
-    # # plt.pause(0.001)
-    # plt.show()
+    #  plt.figure(1)
+    #  #  plt.clf()
+    #  plt.imshow(x_hat[0].detach().cpu().numpy().transpose(1,2,0), cmap='gray')
+    #  #  plt.pause(0.001)
+    #  plt.show()
 
 env.close()
 exit()
